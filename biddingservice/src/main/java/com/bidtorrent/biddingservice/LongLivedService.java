05917a358f50d2a0deb1930f7401730ae9ff13e7
Xavier NOELLE <x.noelle@criteo.com>
2015-07-02 17:01:30 +0200

    Remove toast debug message on service startup

--- biddingservice/src/main/java/com/bidtorrent/biddingservice/LongLivedService.java
+++ biddingservice/src/main/java/com/bidtorrent/biddingservice/LongLivedService.java
@@ -40,8 +40,6 @@ public abstract class LongLivedService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
-
         // For each start request, send a message to start a job and deliver the
         // start ID so we know which request we're stopping when we finish the job
         Message msg = mServiceHandler.obtainMessage();
