9cc697f96c95066a20d8fc062eac878a1f186ee1
Xavier NOELLE <x.noelle@criteo.com>
2015-07-08 18:54:45 +0200

    Stop polling for the window onload state, post when it's done instead

--- biddingservice/src/main/java/com/bidtorrent/biddingservice/receivers/PrefetchReceiver.java
+++ biddingservice/src/main/java/com/bidtorrent/biddingservice/receivers/PrefetchReceiver.java
@@ -26,78 +26,86 @@ public class PrefetchReceiver extends BroadcastReceiver {
         this.handler = handler;
     }
 
-    private void sendPrefetchedPage(final WebView view, final Context context, final Intent intent)
-    {
-        final File tempFile;
-
-        try {
-            tempFile = File.createTempFile("bidtorrent", ".mht", context.getCacheDir());
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        view.saveWebArchive(tempFile.getAbsolutePath(),
-                false, new ValueCallback<String>() {
-                    @Override
-                    public void onReceiveValue(String value) {
-                        Intent auctionIntent;
-                        auctionIntent = new Intent(context, BiddingIntentService.class);
-                        auctionIntent.setAction(Constants.FILL_PREFETCH_BUFFER_ACTION);
-                        auctionIntent.putExtra(Constants.PREFETCHED_CREATIVE_FILE_ARG, value)
-                                .putExtra(Constants.BID_OPPORTUNITY_ARG,
-                                        intent.getStringExtra(Constants.BID_OPPORTUNITY_ARG))
-                                .putExtra(Constants.NOTIFICATION_URL_ARG,
-                                        intent.getStringExtra(Constants.NOTIFICATION_URL_ARG))
-                                .putExtra(Constants.AUCTION_ID_ARG,
-                                        intent.getLongExtra(Constants.AUCTION_ID_ARG, -1));
-
-
-                        context.startService(auctionIntent);
-                        view.destroy();
-                    }
-                });
-    }
-
     @Override
     public void onReceive(final Context context, final Intent intent) {
         Bundle extras = intent.getExtras();
         String creative = extras.getString(Constants.CREATIVE_CODE_ARG);
         final WebView webView = new WebView(context);
-        final JavaScriptReadyListener jsListener = new JavaScriptReadyListener();
+        final JavaScriptReadyListener jsListener = new JavaScriptReadyListener(this.handler, webView, context, intent);
 
         webView.getSettings().setJavaScriptEnabled(true);
         webView.addJavascriptInterface(jsListener, "jslistener");
 
-        this.handler.post(new Runnable() {
+        webView.loadData(creative, "text/html", "utf-8");
+
+        webView.setWebViewClient(new WebViewClient() {
             @Override
-            public void run() {
-                if (jsListener.isDone())
-                    sendPrefetchedPage(webView, context, intent);
-                else
-                    handler.postDelayed(this, 500);
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                view.loadUrl("javascript:window.onLoad=function(data){ jslistener.setDone() }");
+                view.loadUrl("javascript:if (/loaded|complete/.test(document.readyState)){ jslistener.setDone() }");
             }
         });
-
-        webView.loadData(creative, "text/html", "utf-8");
-        webView.loadUrl("javascript:window.onLoad=function(data){ jslistener.setDone() }");
-        webView.loadUrl("javascript:if (/loaded|complete/.test(document.readyState)){ jslistener.setDone() }");
     }
 
     private class JavaScriptReadyListener {
-        private boolean done;
+        private final Handler handler;
+        private final WebView webView;
+        private final Context context;
+        private final Intent intent;
+        private AtomicBoolean done;
 
-        private JavaScriptReadyListener() {
-            this.done = false;
+        private JavaScriptReadyListener(Handler handler, WebView webView, Context context, Intent intent) {
+            this.handler = handler;
+            this.webView = webView;
+            this.context = context;
+            this.intent = intent;
+            this.done = new AtomicBoolean(false);
         }
 
         @JavascriptInterface
         public void setDone(){
-            this.done = true;
+            if (!this.done.getAndSet(true)) {
+                this.sendPrefetchedPage();
+            }
         }
 
-        public boolean isDone() {
-            return done;
+        private void sendPrefetchedPage() {
+            final File tempFile;
+
+            try {
+                tempFile = File.createTempFile("bidtorrent", ".mht", context.getCacheDir());
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+
+            handler.post(
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            webView.saveWebArchive(tempFile.getAbsolutePath(),
+                                    false, new ValueCallback<String>() {
+                                        @Override
+                                        public void onReceiveValue(String value) {
+                                            Intent auctionIntent;
+                                            auctionIntent = new Intent(context, BiddingIntentService.class);
+                                            auctionIntent.setAction(Constants.FILL_PREFETCH_BUFFER_ACTION);
+                                            auctionIntent.putExtra(Constants.PREFETCHED_CREATIVE_FILE_ARG, value)
+                                                    .putExtra(Constants.BID_OPPORTUNITY_ARG,
+                                                            intent.getStringExtra(Constants.BID_OPPORTUNITY_ARG))
+                                                    .putExtra(Constants.NOTIFICATION_URL_ARG,
+                                                            intent.getStringExtra(Constants.NOTIFICATION_URL_ARG))
+                                                    .putExtra(Constants.AUCTION_ID_ARG,
+                                                            intent.getLongExtra(Constants.AUCTION_ID_ARG, -1));
+
+
+                                            context.startService(auctionIntent);
+                                            webView.destroy();
+                                        }
+                                    });
+                        }
+                    });
         }
     }
 }
