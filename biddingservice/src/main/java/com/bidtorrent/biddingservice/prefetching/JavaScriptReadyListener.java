aa378613e0e90f77a668565a51e26b95f62eecfa
Xavier NOELLE <x.noelle@criteo.com>
2015-07-10 12:07:23 +0200

    Inject the sanity check code into the webview at onload time

--- biddingservice/src/main/java/com/bidtorrent/biddingservice/prefetching/JavaScriptReadyListener.java
+++ biddingservice/src/main/java/com/bidtorrent/biddingservice/prefetching/JavaScriptReadyListener.java
@@ -1,83 +1,27 @@
 package com.bidtorrent.biddingservice.prefetching;
 
 import android.webkit.JavascriptInterface;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebResourceResponse;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class JavaScriptReadyListener {
-
-    private static final String IMAGE_FUNCTIONS = "javascript:function isImageOk(img) {\n" +
-            "  if (!img.complete)\n" +
-            "    return false;\n" +
-            "\n" +
-            "  if (typeof img.naturalWidth != \"undefined\" && img.naturalWidth == 0)\n" +
-            "    return false;\n" +
-            "\n" +
-            "  return true;\n" +
-            "}\n" +
-            "\n" +
-            "function allImagesOK()\n" +
-            "{\n" +
-            "  var failedImages = 0;\n" +
-            "\n" +
-            "  for (var i = 0; i < document.images.length; i++) {\n" +
-            "    if (!isImageOk(document.images[i]))\n" +
-            "      failedImages += 1;\n" +
-            "  }\n" +
-            "  \n" +
-            "  return (failedImages == 0);\n" +
-            "}\n";
-
-    private static final String READY_MONITOR_FUNCTION = "javascript:var interval = setInterval(function() { \n" +
-            "  if (allImagesOK()){\n" +
-            "    window.clearInterval(interval);\n" +
-            "    jsListener.setDone();\n" +
-            "  }" +
-            "}, 100);";
-
-    private AtomicBoolean done;
     private Runnable pageSaver;
 
-    private JavaScriptReadyListener(Runnable pageSaver) {
+    public JavaScriptReadyListener(Runnable pageSaver) {
         this.pageSaver = pageSaver;
-        this.done = new AtomicBoolean(false);
     }
 
     @JavascriptInterface
-    public void setDone(){
-        if (!this.done.getAndSet(true)) {
-            this.sendPrefetchedPage();
-        }
+    public void notifyLoadingError()
+    {
+        // FIXME: do something meaningful
+        System.out.println("ERROR");
+    }
+
+    @JavascriptInterface
+    public void notifyLoaded(){
+        this.sendPrefetchedPage();
     }
 
     private void sendPrefetchedPage() {
         this.pageSaver.run();
-    }
-
-    public static void hookReadyEvent(WebView webView, Runnable onDoneEvent){
-        final JavaScriptReadyListener jsListener = new JavaScriptReadyListener(onDoneEvent);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
-        webView.addJavascriptInterface(jsListener, "jsListener");
-
-        webView.setWebViewClient(new WebViewClient() {
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                view.loadUrl(IMAGE_FUNCTIONS);
-                view.loadUrl(READY_MONITOR_FUNCTION);
-
-            }
-
-            @Override
-            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
-                return super.shouldInterceptRequest(view, request);
-            }
-        });
     }
 }
--- biddingservice/src/main/java/com/bidtorrent/biddingservice/receivers/PrefetchReceiver.java
+++ biddingservice/src/main/java/com/bidtorrent/biddingservice/receivers/PrefetchReceiver.java
@@ -16,12 +16,53 @@ import com.bidtorrent.biddingservice.BiddingIntentService;
 import com.bidtorrent.biddingservice.Constants;
 import com.bidtorrent.biddingservice.prefetching.JavaScriptReadyListener;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 public class PrefetchReceiver extends BroadcastReceiver {
 
     private final Handler handler;
+    private static final String jsListenerName = "jsListener";
+    private static final String loadingTrackingJavascript = getloadingTrackingJavascript(jsListenerName);
+
+    private static String getloadingTrackingJavascript(String jsListenerName)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("window.onload = function()")
+                .append("{")
+                .append("for (var i = 0; i < document.images.length; i++) {")
+                .append("  if (document.images[i].naturalWidth == 0)")
+                .append("  {")
+                .append("    " + jsListenerName + ".notifyLoadingError();")
+                .append("    return;")
+                .append("  }")
+                .append("}")
+                .append(jsListenerName + ".notifyLoaded();")
+                .append("}");
+
+        return sb.toString();
+    }
 
     public PrefetchReceiver(Handler handler) {
         this.handler = handler;
@@ -32,15 +73,39 @@ public class PrefetchReceiver extends BroadcastReceiver {
         Bundle extras = intent.getExtras();
         String creative = extras.getString(Constants.CREATIVE_CODE_ARG);
         final WebView webView = new WebView(context);
-        JavaScriptReadyListener.hookReadyEvent(
-                webView,
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        sendPrefetchedData(context, webView, intent);
-                    }
-                });
-        webView.loadData(creative, "text/html", "utf-8");
+        final String instrumentedCreativeCode;
+        final JavaScriptReadyListener jsListener = new JavaScriptReadyListener(new Runnable()
+        {
+            @Override
+            public void run() {
+                sendPrefetchedData(context, webView, intent);
+            }
+        });
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webView.addJavascriptInterface(jsListener, jsListenerName);
+
+        instrumentedCreativeCode = this.injectLoadingTrackingJavascriptCode(creative);
+        if (instrumentedCreativeCode == null)
+        {
+            // FIXME: do something, Bobby!
+            return;
+        }
+
+        webView.loadData(instrumentedCreativeCode, "text/html", "utf-8");
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                return super.shouldInterceptRequest(view, request);
+            }
+        });
     }
 
     private void sendPrefetchedData(final Context context, final WebView webView, final Intent intent) {
@@ -67,16 +132,100 @@ public class PrefetchReceiver extends BroadcastReceiver {
                                         auctionIntent.putExtra(Constants.PREFETCHED_CREATIVE_FILE_ARG, value)
                                                 .putExtra(Constants.BID_OPPORTUNITY_ARG,
                                                         intent.getStringExtra(Constants.BID_OPPORTUNITY_ARG))
-                                        .putExtra(Constants.NOTIFICATION_URL_ARG,
-                                                intent.getStringExtra(Constants.NOTIFICATION_URL_ARG))
-                                        .putExtra(Constants.AUCTION_ID_ARG,
-                                                intent.getLongExtra(Constants.AUCTION_ID_ARG, -1));
+                                                .putExtra(Constants.NOTIFICATION_URL_ARG,
+                                                        intent.getStringExtra(Constants.NOTIFICATION_URL_ARG))
+                                                .putExtra(Constants.AUCTION_ID_ARG,
+                                                        intent.getLongExtra(Constants.AUCTION_ID_ARG, -1));
 
 
-                                context.startService(auctionIntent);
-                            }
-                        });
+                                        context.startService(auctionIntent);
+                                    }
+                                });
                     }
                 });
+    }
+
+    private String injectLoadingTrackingJavascriptCode(String creativeCode) {
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder;
+        Document parsedDocument;
+        InputSource is = new InputSource();
+        Node scriptNode;
+
+        try {
+            documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        is.setCharacterStream(new StringReader(creativeCode));
+
+        try {
+            parsedDocument = documentBuilder.parse(is);
+        } catch (SAXException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        scriptNode = this.createScriptNode(parsedDocument);
+
+        if (scriptNode == null)
+            return null;
+
+        scriptNode.setTextContent(loadingTrackingJavascript);
+
+        return xmlToString(parsedDocument);
+    }
+
+    private static String xmlToString(Document doc)
+    {
+        DOMSource source = new DOMSource(doc);
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer;
+        StringWriter writer = new StringWriter();
+        StreamResult result = new StreamResult(writer);
+
+        try {
+            transformer = tf.newTransformer();
+            transformer.transform(source, result);
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+
+        return writer.toString();
+    }
+
+    private Node createScriptNode(Document parsedDocument) {
+        Node headElement;
+
+        headElement = this.getHeadElement(parsedDocument);
+        if (headElement == null)
+            return null;
+
+        return headElement.appendChild(parsedDocument.createElement("script"));
+    }
+
+    private Node getHeadElement(Document parsedDocument) {
+        NodeList headElements = parsedDocument.getElementsByTagName("head");
+
+        // No head element, try to create it
+        if (headElements.getLength() == 0)
+        {
+            NodeList htmlElements = parsedDocument.getElementsByTagName("html");
+
+            // It's not even HTML, rage quit!
+            if (htmlElements.getLength() == 0)
+                return null;
+
+            return htmlElements.item(0).appendChild(parsedDocument.createElement("head"));
+        }
+        else
+            return headElements.item(0);
     }
 }
